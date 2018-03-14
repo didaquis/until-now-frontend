3263bd014935a37cd63b9e53c1c466cbb7487514
Dídac García <didaquis@gmail.com>
2018-03-14 13:41:24 +0100

    :pencil: Implementar módulo client API

--- src/components/Collections.js
+++ src/components/Collections.js
@@ -7,20 +7,49 @@ import CollectionListed from './CollectionListed';
 import ModalDelete from './ModalDelete';
 import NoData from './NoData';
 
-function Collections(props) {
-	return (
-		<section>
-			<Header title={'Collections'} />
-			<PanelNewCollection />
-			<ButtonNewElement text={'New collection'} />
-			<HrElement />
-			<div className="row">
-				<CollectionListed title={'ABC'} count={'7'} />
-			</div>
-			<NoData />
-			<ModalDelete textForBody={'Are you sure you want to delete this item?'}/>
-		</section>
-	)
+import untilNowApi from '../utils/until-now-api-client';
+
+class Collections extends React.Component{
+	constructor(){
+		super();
+		this.state = {
+			list: []
+		};
+	}
+
+	componentWillMount(){
+		untilNowApi.listCollections()
+			.then( res => this.refreshList(res) )
+			.catch(error => console.error(error));
+	}
+
+	refreshList = (results) => {
+		if(results.status === 'OK'){
+			this.setState({list:results.data});
+		}
+	}
+
+	render() {
+		return (
+			<section>
+				<Header title={'Collections'} />
+				<PanelNewCollection />
+				<ButtonNewElement text={'New collection'} />
+				<HrElement />
+				<div className="row">
+					{
+						this.state.list.map( (collection) =>{
+							return (
+								<CollectionListed key={collection._id} title={collection.name} count={'7'} />
+							)
+						} )
+					}
+				</div>
+				<NoData />
+				<ModalDelete textForBody={'Are you sure you want to delete this item?'}/>
+			</section>
+		)
+	}
 }
 
 export default Collections;
