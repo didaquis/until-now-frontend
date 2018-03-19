0f1568e7d4e8c0d74f79ec933e179b16548966af
Dídac García <didaquis@gmail.com>
2018-03-19 17:35:44 +0100

    :sparkles: Ya es posible crear colecciones

--- src/components/PanelNewCollection.js
+++ src/components/PanelNewCollection.js
@@ -1,14 +1,32 @@
 import React from 'react';
 
+import untilNowApi from '../utils/until-now-api-client';
+
 class PanelNewCollection extends React.Component {
 	constructor() {
 		super();
 		this.state = {
-			
+			nameCollectionInput: ''
 		}
 	}
 
+	fillInput = (input) => {
+		if(input.id === 'nameCollectionInput'){
+			this.setState({ nameCollectionInput: input.value });
+		}
+	}
 
+	handlerSubmit = () =>{
+		untilNowApi.createCollection(this.state.nameCollectionInput, '5aa6bb9e341a690ff909faee')
+			.then(res => {
+				if(res.status === 'OK'){
+					window.location.reload();
+				}else{
+					console.log(res)
+				}
+			})
+			.catch(err => console.error(err))
+	}
 
 	render() {
 		if (this.props.show) {
@@ -19,12 +37,12 @@ class PanelNewCollection extends React.Component {
 							<div className="card">
 								<div className="card-body">
 									<h4 className="card-title">Add new collection</h4>
-									<form>
+									<form onSubmit={(e) => { e.preventDefault(); this.handlerSubmit() }}>
 										<div className="form-group">
 											<label htmlFor="nameCollectionInput">Name of collection</label>
 											<input type="text" className="form-control" id="nameCollectionInput" aria-describedby="nameCollectionHelp" placeholder="Enter name of collection"
-												required pattern="[A-Za-z0-9.\-_\*\/\|]{3,}" autoFocus />
-											<small id="nameCollectionHelp" className="form-text text-muted">At least 3 chars</small>
+												required pattern="[A-Za-z0-9.\-_\*\/\|]{3,}" autoFocus onChange={e => this.fillInput(e.target)} value={this.state.nameCollectionInput} />
+											<small id="nameCollectionHelp" className="form-text text-muted">At least 3 chars. Spaces are not valid chars</small>
 										</div>
 										<button onClick={ (e) => { e.preventDefault(); this.props.onClick() }} className="btn btn-secondary">Close</button>
 										<button type="submit" className="btn btn-success float-right">Create</button>
