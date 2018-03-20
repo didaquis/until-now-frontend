128bc89625d56afe24e881b4affc63389b2e66d4
Dídac García <didaquis@gmail.com>
2018-03-20 22:55:30 +0100

    :pencil: Añadidos textos descripción del proyecto

--- src/components/MainHeader.js
+++ src/components/MainHeader.js
@@ -5,8 +5,10 @@ function MainHeader(props){
 		<div className="row">
 			<div className="col">
 				<h1 className="text-center mb-4">Until Now</h1>
-				<p className="lead">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum nihil expedita rem molestiae aperiam corrupti vitae accusamus ab a qui autem beatae et quam, consectetur cupiditate sequi alias ipsa molestias!</p>
-				<p className="lead">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum nihil expedita rem molestiae aperiam corrupti vitae accusamus ab a qui autem beatae et quam, consectetur cupiditate sequi alias ipsa molestias!</p>
+				<p className="lead text-center">In some activities, such as diving or rock climbing specialized technical equipment is designed and used only for that purpose.
+				<br/>
+				The manufacturers have established , for security reasons, that these products have a maximum lifespan duration. In fact, exceeding the useful life of these elements used in risk activities can have fatal consequences.</p>
+				<p className="lead text-center"><em>Until Now</em> helps you manage the renewal date of technical equipment. The app enables you to quickly display which gear you should soon discard or/and renew.</p>
 			</div>
 		</div>
 	)
