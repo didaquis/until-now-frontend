3585bb5b12f3d4d1803de5e34ff41b63cfd86edd
Dídac García <didaquis@gmail.com>
2018-03-20 23:45:31 +0100

    :recycle: Preparando componentes para recibir contenido

--- src/components/Features.js
+++ src/components/Features.js
@@ -12,8 +12,8 @@ function Features(props){
 			<hr className="featurette-divider mt-5 mb-5" />
 			<div className="row featurette">
 				<div className={`col-md-6 ${reverseClassNameForText}`}>
-					<h2 className="featurette-heading">Oh yeah, it's that good. <span className="text-muted">See for yourself.</span></h2>
-					<p className="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+					<h2 className="featurette-heading">{props.title} <span className="text-muted">{props.subtitle}</span></h2>
+					<p className="lead">{props.text}</p>
 				</div>
 				<div className={`col-md-6 ${reverseClassNameForPicture}`}>
 					<img className="featurette-image img-fluid mx-auto rounded" alt="feature" src={props.img} data-holder-rendered="true" />
--- src/components/Home.js
+++ src/components/Home.js
@@ -7,8 +7,20 @@ function Home(props) {
 	return (
 		<section>
 			<MainHeader />
-			<Features reverse={true} img={require('../images/features_1.png')} />
-			<Features reverse={false} img={require('../images/features_2.png')} />
+			<Features 
+				reverse={true} 
+				title={'Oh yeah, it\'s that good.'} 
+				subtitle={'See for yourself.'} 
+				text={'Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.'} 
+				img={require('../images/features_1.png')} 
+			/>
+			<Features 
+				reverse={false} 
+				title={'Oh yeah, it\'s that good.'} 
+				subtitle={'See for yourself.'} 
+				text={'Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.'} 
+				img={require('../images/features_2.png')} 
+			/>
 		</section>
 	)
 }
