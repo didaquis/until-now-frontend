b3ba0cb786a3200fae653463514fe325225f82e0
Dídac García <didaquis@gmail.com>
2018-03-21 12:37:27 +0100

    :recycle: Refactorizar código componente principal

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Switch, Route, HashRouter } from 'react-router-dom';
 
 import Nav from './components/Nav';
@@ -9,23 +9,21 @@ import Footer from './components/Footer';
 import Error404 from './components/Error404';
 
 
-class App extends Component {
-	render() {
-		return (
-			<HashRouter>
-				<div className="container-fluid p-4">
-					<Nav />
-					<Switch>
-						<Route exact path="/" component={Home} />
-						<Route exact path="/collections" component={Collections} />
-						<Route path="/collection/:idOfCollection" component={Items} />
-						<Route component={Error404} />
-					</Switch>
-					<Footer />
-				</div>
-			</HashRouter>
-		);
-	}
-}
+const App = () => {
+	return (
+		<HashRouter>
+			<div className="container-fluid p-4">
+				<Nav />
+				<Switch>
+					<Route exact path="/" component={Home} />
+					<Route exact path="/collections" component={Collections} />
+					<Route path="/collection/:idOfCollection" component={Items} />
+					<Route component={Error404} />
+				</Switch>
+				<Footer />
+			</div>
+		</HashRouter>
+	);
+};
 
 export default App;
