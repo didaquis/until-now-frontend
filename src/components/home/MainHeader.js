300ba092f62ff33db5293f9f1e16acaa243c448f
Dídac García <didaquis@gmail.com>
2018-03-21 13:02:12 +0100

    :recycle: Refactorizando código

--- src/components/home/MainHeader.js
+++ src/components/home/MainHeader.js
@@ -1,17 +1,18 @@
 import React from 'react';
 
-function MainHeader(props){
-	return(
+function MainHeader() {
+	return (
 		<div className="row">
 			<div className="col">
 				<h1 className="text-center mb-4">Until Now</h1>
 				<p className="lead text-center">In some activities, such as diving or rock climbing specialized technical equipment is designed and used only for that purpose.
-				<br/>
-				The manufacturers have established , for security reasons, that these products have a maximum lifespan duration. In fact, exceeding the useful life of these elements used in risk activities can have fatal consequences.</p>
+					<br />
+				The manufacturers have established , for security reasons, that these products have a maximum lifespan duration. In fact, exceeding the useful life of these elements used in risk activities can have fatal consequences.
+				</p>
 				<p className="lead text-center"><em>Until Now</em> helps you manage the renewal date of technical equipment. The app enables you to quickly display which gear you should soon discard or/and renew.</p>
 			</div>
 		</div>
-	)
+	);
 }
 
 export default MainHeader;
