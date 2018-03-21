85025bba209b5fbfdc2c969db5d7350e6dd75daa
Dídac García <didaquis@gmail.com>
2018-03-21 22:35:15 +0100

    :pencil: Incorporo un trick para despertar al servidor

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -2,26 +2,37 @@ import React from 'react';
 import MainHeader from './MainHeader';
 import Features from './Features';
 
-function Home() {
-	return (
-		<section>
-			<MainHeader />
-			<Features
-				reverse
-				title={'Oh yeah, it\'s that good.'}
-				subtitle={'See for yourself.'}
-				text={'Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.'}
-				img={require('../../images/features_1.png')}
-			/>
-			<Features
-				reverse={false}
-				title={'Oh yeah, it\'s that good.'}
-				subtitle={'See for yourself.'}
-				text={'Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.'}
-				img={require('../../images/features_2.png')}
-			/>
-		</section>
-	);
+import untilNowApi from '../../utils/until-now-api-client';
+
+class Home extends React.Component {
+	componentWillMount() {
+		// This is a trick for wake up the server.
+		untilNowApi.ping()
+			.then()
+			.catch();
+	}
+
+	render() {
+		return (
+			<section>
+				<MainHeader />
+				<Features
+					reverse
+					title={'Oh yeah, it\'s that good.'}
+					subtitle={'See for yourself.'}
+					text={'Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.'}
+					img={require('../../images/features_1.png')}
+				/>
+				<Features
+					reverse={false}
+					title={'Oh yeah, it\'s that good.'}
+					subtitle={'See for yourself.'}
+					text={'Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.'}
+					img={require('../../images/features_2.png')}
+				/>
+			</section>
+		);
+	}
 }
 
 export default Home;
