429d9ce9e297b17568007333a68278ad4f5c5a88
Dídac García <didaquis@gmail.com>
2018-03-22 23:48:48 +0100

    :art: Mejoras de visualización

--- src/components/NoData.js
+++ src/components/NoData.js
@@ -3,7 +3,7 @@ import React from 'react';
 function NoData() {
 	return (
 		<div className="row">
-			<div className="col">
+			<div className="col mt-4 mb-4">
 				<h4 className="text-center">{"You don't have data yet"}</h4>
 			</div>
 		</div>
