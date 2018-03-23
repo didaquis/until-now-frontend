1f1307305a7dbb26f306e562c5e92b0cc19323ca
Dídac García <didaquis@gmail.com>
2018-03-24 00:51:15 +0100

    :art: Arreglar espaciado entre lineas en titulares

--- src/components/home/Features.js
+++ src/components/home/Features.js
@@ -13,11 +13,17 @@ function Features(props) {
 			<hr className="featurette-divider mt-5 mb-5" />
 			<div className="row featurette">
 				<div className={`col-md-6 ${reverseClassNameForText}`}>
-					<h2 className="featurette-heading">{props.title} <span className="text-muted">{props.subtitle}</span></h2>
+					<h2 className="featurette-heading">{props.title} </h2>
+					<h2><span className="text-muted">{props.subtitle}</span></h2>
 					<p className="lead">{props.text}</p>
 				</div>
 				<div className={`col-md-6 ${reverseClassNameForPicture}`}>
-					<img className="featurette-image img-fluid mx-auto rounded" alt="feature" src={props.img} data-holder-rendered="true" />
+					<img
+						className="featurette-image img-fluid mx-auto rounded"
+						alt="feature"
+						src={props.img}
+						data-holder-rendered="true"
+					/>
 				</div>
 			</div>
 		</section>
